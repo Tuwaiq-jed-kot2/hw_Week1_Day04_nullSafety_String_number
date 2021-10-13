4df8dd2fe6a3700e8bba88729227bfb24e996305
ASUS <EshraqAlmalki>
2021-10-14 01:50:26 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,3 +1,41 @@
-fun main(){
+fun main() {
+    fun getLength(str: String?): Int {
+        var getLength: String = ""
+
+
+        if (str == null) {
+            return 0
+        } else {
+            return str.length
+        }
+
+
+    }
+
+
+    fun main() {
+        val str1 = "The quick brown fox jumps over the lazy dog"
+        val startIndex = 10
+        val endIndex = 24
+        val substring = str1.subSequence(startIndex, endIndex)
+        println("the substring is :" +substring)
+
+
+
+    }
+
+
+    fun main() {
+        fun addNum(num1 : Double) :Double {
+
+            return (5* (212.0-32)) / 9
+        }
+
+
+    } //i try my best as you toled me . sorry if my solutions disappointed you guys :(
+
+
+
+
 
 }
