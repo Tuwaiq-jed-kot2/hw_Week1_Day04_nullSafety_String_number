d3ee9da612ae544f840e4bc193cef727ba5d185d
HumaidALKhathami <humaid.ali.jed.k2@tuwaiq.edu.sa>
2021-10-13 20:08:17 +0300

    great homework

--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -2,11 +2,23 @@ import java.lang.NumberFormatException
 
 fun main() {
     try {
-        print("type the temperature in fahrenheit: ")
-        val temperature = Integer.valueOf(readLine())
-        val newTemperature = (5*(temperature-32)) / 9
+        val formatter = "%.4f"
+        print("type the temperature: ")
+        val temperature :Float = Integer.valueOf(readLine()).toFloat()
+        print("type the unit of your temperature (c or f only): ")
+        val unit = readLine().toString()
+        if (unit == "f") {
+            val newTemperature : Float = ((5 * (temperature - 32)) / 9)
+            println("$temperature degree Fahrenheit is equal to $newTemperature in Celsius")
+        }
+        else if (unit == "c") {
+            val newTemperature = ((( 9 * temperature) / 5 ) +32).toFloat()
+            println("${formatter.format(temperature)} degree Celsius is equal to ${formatter.format(newTemperature)} in Fahrenheit")
 
-        println("$temperature degree Fahrenheit is equal to $newTemperature in Celsius")
+        }else {
+            println("you entered inconvenient unit, please rerun the program and enter convenient unit")
+        }
+
     }catch (e: NumberFormatException){
         println("you entered a letter, please rerun the program and enter numbers only")
     }
