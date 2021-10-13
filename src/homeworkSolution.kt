2588a6bc014d552470c0e03abe353d5dd28defc5
FAISALT1000 <FAISAL.FAHAD.jed.k2@tuwaiq.edu.sa>
2021-10-14 00:14:03 +0300

    Thank you for the homework :>

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,3 +1,34 @@
-fun main(){
-
+//Part I - Null Safety.
+fun main() {
+    getLength()
 }
+fun getLength(){
+    val str: String? = null
+    // val str: String? = readLine()
+    if (str != null && str.length > 0) {
+        print(str+" \nString of length ${str.length}")
+    } else if(str==null) {
+
+        print("0 String of length ")
+    }else{
+/*  */
+        print("Erorr")
+    }}
+
+
+
+/*
+fun getLength(str: String?) : Int {
+ //var getLength: String? =null
+var str1 =5
+
+    if (str == null) {
+        return 0
+        println(0)
+    } else {
+        return str.length
+    }
+
+    // return str.length
+
+}*/
