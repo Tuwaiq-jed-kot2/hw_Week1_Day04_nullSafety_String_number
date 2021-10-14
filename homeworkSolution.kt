22dc3af21b62da7b4a0978f7b8f0ae4546700186
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-10-14 03:12:43 +0300

    LL

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,14 @@
 fun main(){
 
+    getSubstring()
+}
+
+val fax = "The quick brown fox jumps over the lazy dog."
+
+fun getSubstring(){
+    val brownFox = fax.substring(10,26)
+    println("old = $fax")
+    println("new = $brownFox")
+
+
 }
--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,3 +1,20 @@
-fun main(){
+package com.includehelp
 
+import java.util.*
+fun main(args: Array<String>) {
+
+
+    //Input Stream
+    val scanner = Scanner(System.`in`)
+
+
+    //Input temperature in Fahrenheit
+    print("Enter temperature into Fahrenheit : ")
+    val fahrenheit = scanner.nextDouble()
+
+    //Convert  Fahrenheit to Celsius
+    val celsius =( (fahrenheit  -  32  ) *  5)/9
+
+    //Print temperature in Celsius
+    println("Temperature in Fahrenheit ($fahrenheit) = Celsius ($celsius)")
 }
