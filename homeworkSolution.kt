9bef315ea8c0ce7a128f6790c695ae8d99b13516
Sara999asar <63171486+Sara999asar@users.noreply.github.com>
2021-10-14 08:36:38 +0300

    THANK YOU

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,53 @@
+
+Answer 1 :
+
+
 fun main(){
 
+
+    println(getLenth("  Hi Iam Sara"))
 }
+
+fun getLenth(str:String?):Int{
+    if (str==null)
+        return 0
+    else
+    //fun getLenth(): Int {
+
+
+        return str.length
+}
+
+
+
+Answer 2 :
+
+fun main(args: Array<String>) {
+    val str1 = "Sara Alghamdi"
+    val str2 = "Good Morning"
+    val startIndex = 0
+    val endIndex = 11
+    val substring = str1.subSequence(startIndex, endIndex)
+    val substring2 = str2.subSequence(startIndex, endIndex)
+    println("My name: " + substring)
+    println("anything: " + substring2)
+}
+
+
+Answer 3 :
+
+
+fun main() {
+    val celsius =addFahrnenhetNum(null)
+    println(celsius)
+}
+fun addFahrnenhetNum(addFahrnenhetNum : Float?):String {
+    if (addFahrnenhetNum==null){
+        return(" fahrenheit degree")
+//if (addFahrnenhetNum==null){
+    }else{
+        return ("$addFahrnenhetNum degrees Fahrenheit = ${((addFahrnenhetNum- 32) *5) /9 }degrees Celsius")
+    }
+
+}
+
