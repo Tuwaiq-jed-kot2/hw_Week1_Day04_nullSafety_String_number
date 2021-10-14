b597fc50acbac914eaee7f95d343d0494ce8a2c4
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-14 03:09:21 +0300

    home work

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,3 +1,34 @@
+
+// part 1
+fun main() {
+    println(getLength("This is a string"))
+
+}
+fun getLength(str: String?): Int {
+    val num1 =
+        if (str != null) str.length
+        else 0
+    return str.length
+}
+
+
+
+}
+
+
+// part 2
 fun main(){
 
+    val old:String = " The quick  brwon  fox jumps over lazy dog"
+    println(old)
+    println(old.substring(10,29))
+
+}
+
+
+//part 3
+fun main() {
+    val fahrenheit:Int?=212
+    val Celsius : Double= 5*(fahrenheit?.toDouble()!!-32)/9
+    println("Celsius $Celsius")
 }
