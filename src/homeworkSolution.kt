9b8e5953a8484673497b6ccbd5743ce017d5b44b
Sirin Alkadi <serenalgadi@gmail.com>
2021-10-23 21:57:26 +0300

    :)

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -5,7 +5,7 @@ fun main(){
     println("old = The quick brown fox jumps over the lazy dog.")
     println("new = ${subStr("The quick brown fox jumps over the lazy dog.")}.")
     //PartIII
-    println(convTemp(212.0))
+    println(convTemp(null))
 }
 //PartI
 fun getLength(str: String?) : Int {
@@ -20,6 +20,12 @@ fun subStr(oldStr:String):String{
     return oldStr.substring(9..25)
 }
 //PartIII
-fun convTemp(F:Double): String {
-    return "$F degree Fahrenheit is equal to ${ ((F - 32) * 5) / 9 } in Celsius."
+fun convTemp(fahrenheit: Double?): String? {
+
+    if (fahrenheit == null){
+        return null
+    }else {
+        return "$fahrenheit degree Fahrenheit is equal to ${((fahrenheit - 32) * 5) / 9} in Celsius."
+    }
+
 }
